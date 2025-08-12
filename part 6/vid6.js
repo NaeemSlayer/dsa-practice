a68b913310b1adbe6955aed521c733d8d2a4bc4d
NaeemSlayer <muhammedalam150@gmail.com>
2025-08-12 16:10:47 +0500

    leetcode 26+88

--- part 6/vid6.js
+++ part 6/vid6.js
@@ -60,8 +60,67 @@
 //   }
 // }
 
-// Q.Remove duplicates from the sorted array
-// Q.Merge sorted array
+// // Leetcode 26. Remove Duplicates from Sorted Array
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var removeDuplicates = function (nums) {
+//     let j = 1
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         if (nums[i] != nums[i + 1]) {
+//             nums[j] = nums[i + 1]
+//             j++
+//         }
+//     }
+//     return j
+// };
+
+// // Q.Merge sorted array (video appraoch using extra space)
+// let arr1 = [1, 3, 5];
+// let arr2 = [2, 4, 6];
+// let merge = new Array(arr1.length + arr2.length);
+// let i = (j = k = 0);
+// while (i < arr1.length && j < arr2.length) {
+//   if (arr1[i] < arr2[j]) {
+//     merge[k++] = arr1[i++];
+//   } else {
+//     merge[k++] = arr2[j++];
+//   }
+// }
+// while (j < arr2.length) {
+//   merge[k++] = arr2[j++];
+// }
+// while (i < arr1.length) {
+//   merge[k++] = arr1[i++];
+// }
+// console.log(merge);
+
+// // LeetCode "Merge Sorted Array" problem (No. 88)
+// /**
+//  * @param {number[]} nums1
+//  * @param {number} m
+//  * @param {number[]} nums2
+//  * @param {number} n
+//  * @return {void} Do not return anything, modify nums1 in-place instead.
+//  */
+// var merge = function (nums1, m, nums2, n) {
+//     let i = m - 1
+//     let j = n - 1
+//     let k = m + n - 1
+//     while (j >= 0) {
+//         if (i >= 0 && nums1[i] > nums2[j]) {
+//             nums1[k] = nums1[i]
+//             i--
+//         } else {
+//             nums1[k] = nums2[j]
+//             j--
+
+//         }
+//         k--
+//     }
+// };
+
 // Q.Best time to buy and sell stocks
 // Q.Sort the color
 // Q.Maximum Subarray /kadane's algorithm
